f8469fce2d87ca2b964eb3158caaa41d392d0d9f
papycoda <opeyemi655@gmail.com>
2024-04-30 01:29:39 +0100

    "Added HomePageView to render index.html in Home app; updated settings.py for static files"
    This commit includes changes to the Home app's views.py file to add a HomePageView function that will render the index

--- prontoback/prontoback/urls.py
+++ prontoback/prontoback/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Home import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',view=views.HomePageView,name="home"),
 ]
