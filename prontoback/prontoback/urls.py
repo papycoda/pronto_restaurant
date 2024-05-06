df6d635c8e3f4503eb21d34bfa5b6864e2a8fc63
papycoda <opeyemi655@gmail.com>
2024-05-06 15:31:53 +0100

    updated urls and base page, fixed static file isues

--- prontoback/prontoback/urls.py
+++ prontoback/prontoback/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from Home import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', views.HomePageView, name="home"),  
+    path('', views.HomePageView, name="home"),  
     path('menu/', views.MenuView, name="menu"),
     path('about/', views.AboutView, name="about"), 
     path('book/', views.BookView, name="book"),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
