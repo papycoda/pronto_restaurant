1470093cac238950bb67ab7917feb69c9e28cc06
papycoda <opeyemi655@gmail.com>
2024-05-19 22:15:23 +0100

    updated pages and menu models

--- prontoback/Home/models.py
+++ prontoback/Home/models.py
@@ -16,7 +16,8 @@ class MenuItem(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=15, decimal_places=2, blank=True, null=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='menu_items')  
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='menu_items') 
+    image_url = models.URLField(blank=True, null=True)
 
     def __str__(self):
         return self.name
