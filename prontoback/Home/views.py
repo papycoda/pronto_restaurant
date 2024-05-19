aed4392c365afee787cdf2a5416728d2c66ac8fc
papycoda <opeyemi655@gmail.com>
2024-05-19 16:32:32 +0100

    updated menu to populate backend

--- prontoback/Home/views.py
+++ prontoback/Home/views.py
@@ -3,13 +3,32 @@ from django.http import HttpResponse
 from openpyxl import load_workbook
 from .models import MenuItem,Category
 from django.contrib.auth.decorators import user_passes_test
+from django.core.paginator import Paginator
 
 
 def HomePageView (request):
     return render(request, 'index.html')
 
 def MenuView (request):
-    return render(request, 'menu.html')
+    categories = Category.objects.prefetch_related('menu_items').order_by('name')
+    selected_category = request.GET.get('category')
+    all_items = MenuItem.objects.all() 
+
+    if selected_category:
+        menu_items = all_items.filter(category__name__iexact=selected_category)
+    else:
+        menu_items = all_items
+
+    paginator = Paginator(all_items, 8)  
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    
+    context = {
+        'categories': categories,
+        'page_obj': page_obj,
+        'selected_category': selected_category,
+    }
+    return render(request, 'menu.html', context) 
 
 def AboutView(request):
     return render(request, 'about.html')
--- prontoback/prontoback/settings.py
+++ prontoback/prontoback/settings.py
@@ -101,6 +101,9 @@ USE_I18N = True
 
 USE_TZ = True
 
+USE_L10N = True  
+USE_THOUSAND_SEPARATOR = True
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
