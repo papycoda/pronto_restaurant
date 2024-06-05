51b4a44540f445d39280c434bfae61c5922b2cea
papycoda <opeyemi655@gmail.com>
2024-06-05 15:48:45 +0100

    "Updated Home models to use ImageKit, removed image files, and added ImageKit to installed apps."

--- prontoback/Home/models.py
+++ prontoback/Home/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFit
 
 class Category(models.Model):
     name = models.CharField(max_length=20, unique=True)
@@ -12,12 +14,19 @@ class Category(models.Model):
     class Meta:
         verbose_name_plural = "Categories"
 
+
+
 class MenuItem(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=15, decimal_places=2, blank=True, null=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='menu_items')
-    image = models.ImageField(upload_to='images/', blank=True, null=True)
+    image = ProcessedImageField(upload_to='images/', 
+                                processors=[ResizeToFit(width=300, height=200)], 
+                                format='JPEG', 
+                                options={'quality': 90}, 
+                                blank=True, 
+                                null=True)
 
     def __str__(self):
         return self.name
