7ebdcb2b9088407eefc273fb82dc63c4f20f005e
papycoda <opeyemi655@gmail.com>
2024-06-06 14:37:03 +0100

    "Updated Home models.py to use custom file storage for images, and modified menu.html template to display images with default category image fallback and no image placeholder."

--- prontoback/Home/models.py
+++ prontoback/Home/models.py
@@ -3,10 +3,17 @@ from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 from imagekit.models import ProcessedImageField
 from imagekit.processors import ResizeToFit
+from django.core.files.storage import FileSystemStorage
+
+class CustomStaticFileStorage(FileSystemStorage):
+    def _save(self, name, content):
+        if self.exists(name):
+            return name  
+        return super()._save(name, content)
 
 class Category(models.Model):
     name = models.CharField(max_length=20, unique=True)
-    default_image = models.ImageField(upload_to='Static/images/', blank=True, null=True)
+    default_image = models.ImageField(upload_to='Static/images/', storage=CustomStaticFileStorage(), blank=True, null=True)
 
     def __str__(self):
         return self.name
@@ -21,7 +28,7 @@ class MenuItem(models.Model):
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=15, decimal_places=2, blank=True, null=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='menu_items')
-    image = ProcessedImageField(upload_to='images/', 
+    image = ProcessedImageField(upload_to='Static/images/', 
                                 processors=[ResizeToFit(width=300, height=200)], 
                                 format='JPEG', 
                                 options={'quality': 90}, 
@@ -102,3 +109,5 @@ class Event(models.Model):
             self.ticket_url = None
         super().save(*args, **kwargs)
         
+
+ 
